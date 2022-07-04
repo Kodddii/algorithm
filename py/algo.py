14a893107730a5eb818a416ab4f6cc664fbc784c
Kodddii <justic521@gmail.com>
2022-07-05 03:04:38 +0900

    Update algo.py

--- py/algo.py
+++ py/algo.py
@@ -1,3 +1,23 @@
+# input = [0, 3, 5, 6, 1, 2, 4]
+
+
+# def find_max_plus_or_multiply(array):
+#     # 이 부분을 채워보세요!
+#     sum = 0
+#     for x in array:
+#         if sum == 0 or sum ==1:
+#             sum = sum+x
+#         elif x == 0 or x ==1 :
+#             sum = sum + x
+#         else:
+#             sum = sum * x
+        
+#     return sum
+
+
+# result = find_max_plus_or_multiply(input)
+# print(result)
+
 input = [0, 3, 5, 6, 1, 2, 4]
 
 
@@ -5,13 +25,10 @@ def find_max_plus_or_multiply(array):
     # 이 부분을 채워보세요!
     sum = 0
     for x in array:
-        if sum == 0:
-            sum = sum+x
-        elif x == 0 or x ==1 :
-            sum = sum + x
+        if x<=1 or sum<=1:
+            sum += x
         else:
-            sum = sum * x
-        
+            sum *= x
     return sum
 
 
